8f9e60f07c66601ff73c753d5b4901aeb85ea843
root <root@guest.guest>
2022-06-12 18:29:05 +0000

    ca va etre douloureux

--- src/app/compo/mainchat/mainchat.component.ts
+++ src/app/compo/mainchat/mainchat.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+// import { io } from 'socket.io-client';
+
+// const SOCKET_ENDPOINT = 'localhost:3000';
 
 @Component({
   selector: 'app-mainchat',
   templateUrl: './mainchat.component.html',
   styleUrls: ['./mainchat.component.css']
 })
-export class MainchatComponent implements OnInit {
+export class MainchatComponent {
 
+  // socket: any;
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  // ngOnInit(): void {
+  //   this.SetupSocketConnection();
+  // }
+
+  // SetupSocketConnection() {
+  //   this.socket = io();
+  // }
 
 }
