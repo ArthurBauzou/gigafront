513bb175a545a2eed39b4f2370d26926d0251d1b
root <root@guest.guest>
2022-06-12 17:25:42 +0000

    premier afficahage

---
+++ src/app/compo/mainchat/mainchat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mainchat',
+  templateUrl: './mainchat.component.html',
+  styleUrls: ['./mainchat.component.css']
+})
+export class MainchatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
