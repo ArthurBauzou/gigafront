e36bf6ef2b1bf17395fe461cbd3aab542d99cd42
arthurbauzou <arthurbauzou@gmail.com>
2022-06-13 16:01:25 +0000

    le front c’est de la merde

--- src/app/compo/mainchat/mainchat.component.ts
+++ src/app/compo/mainchat/mainchat.component.ts
@@ -8,6 +8,36 @@ import { SocketioService } from 'src/app/socketio.service';
 })
 export class MainchatComponent implements OnInit, OnDestroy {
 
+  userlist = [];
+  user = {
+    username: 'Arthur',
+    avatar: '../assets/images/chatriste.jpg'
+  };
+  
+  decoyMess1 = {
+    auteur: "lebron",
+    color: "#C70039",
+    avatar: "../assets/images/LeBron_James.jpg",
+    body: "C’est pas vrai il y a encore des fourmis !",
+    date: "28 juin 2022, 10h13m15s"
+  }
+  decoyMess2 = {
+    auteur: "waaaaluigi",
+    color: "#6d29ba",
+    avatar: "../assets/images/waluigi.jpg",
+    body: "Je te l’avais bien dit de faire attention à pas renverser la confiture gros dégeulasse",
+    date: "28 juin 2022, 10h13m15s"
+  }
+  decoyMess3 = {
+    auteur: "lebron",
+    color: "#C70039",
+    avatar: "../assets/images/LeBron_James.jpg",
+    body: "Mais j’ai du étaler la confiotte avec une de tes fourchettes, si tu faisais la vaisselle aussi",
+    date: "28 juin 2022, 10h13m15s"
+  }
+
+  messages = [this.decoyMess1, this.decoyMess2, this.decoyMess3];
+
   constructor(private _socketService: SocketioService) { }
 
   ngOnInit(): void {
