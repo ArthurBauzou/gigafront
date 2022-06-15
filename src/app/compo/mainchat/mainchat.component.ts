bc226b446e8eddddcd1a2a45aa734b772305201e
arthurbauzou <arthurbauzou@gmail.com>
2022-06-15 16:56:58 +0000

    in and out quand c’est avec le bouton

--- src/app/compo/mainchat/mainchat.component.ts
+++ src/app/compo/mainchat/mainchat.component.ts
@@ -1,8 +1,7 @@
-import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Message } from 'src/app/models/message.model';
 import { SocketioService } from 'src/app/socketio.service';
-
 
 @Component({
   selector: 'app-mainchat',
@@ -12,27 +11,32 @@ import { SocketioService } from 'src/app/socketio.service';
 export class MainchatComponent implements OnInit, OnDestroy {
   
   colorlist = ['#ff2a2aff','#ff6600ff','#ffcc00ff','#44aa00ff','#00aad4ff','#0000aaff','#7137c8ff','#c837abff']
-  username= 'Arthur'
-  usercolor= ''
-  messageSub: Subscription;
+  user = {
+    id: '',
+    name: '',
+    color: ''
+  }
   messages: Message[] = []
-  userlist= [
-    {
-      username: "PascalBrutal",
-      color: '#ff6600ff'
-    },
-    {
-      username: "Octoblubos",
-      color: '#00aad4ff'
-    }
-  ]
+  userlist: any[] = []
+
+  messageSub: Subscription;
+  usersConnectedSub: Subscription;
   
   @ViewChild('messageB') textarea!:ElementRef
+
+  @HostListener('window:beforeunload', [ '$event' ])
+  unloadHandler() {
+    this._socketService.userDeco(this.user)
+    this._socketService.disconnect()
+  }
 
   constructor(private _socketService: SocketioService) { 
     this.messageSub = this._socketService.watchMessages().subscribe((m) => {
       this.messages.push(m)
     });
+    this.usersConnectedSub = this._socketService.watchUsers().subscribe((list) => {
+      this.userlist = list;
+    })
    }
   
   // fonctions automatiques du cysle de vie des composants
@@ -44,52 +48,65 @@ export class MainchatComponent implements OnInit, OnDestroy {
     this.iniTextarea()
   }
   ngOnDestroy(): void {
+    this._socketService.userDeco(this.user)
     this._socketService.disconnect()
   }
 
   adjustHeight(el:HTMLElement){
     el.style.height = "1px";
-    console.log((el as HTMLInputElement).value)
     el.style.height = (el.scrollHeight - 16)+"px";
     if ((el as HTMLInputElement).value == '') {el.style.height = "18px"}
   }
 
   // fonctions d’input
   loguser(form:any) {
-    this.username = form.value.username
+    // USERNAME
+    this.user.name = form.value.username
+    // ID
+    function dec2hex (dec:number) {
+      return dec.toString(16).padStart(2, "0")
+    }
+    function generateId (len?:number) {
+      var arr = new Uint8Array((len || 40) / 2)
+      window.crypto.getRandomValues(arr)
+      return Array.from(arr, dec2hex).join('')
+    }
+    this.user.id = generateId()
+    // COLOR
+    this.user.color = this.colorlist[Math.floor(Math.random()*this.colorlist.length)]
+    document.getElementById('colorpicker')!.style.backgroundColor = this.user.color
+
+    this._socketService.userCo(this.user)
+  }
+  logout() {
+    this._socketService.userDeco(this.user)
+    this.user = {id:'',name:'',color:''};
   }
   sendMessage(form:any) {
     if (form.value.message != '') {
-      let message = new Message(this.username, this.usercolor, form.value.message)
+      let message = new Message(this.user.name, this.user.color, form.value.message)
       form.reset();
       this._socketService.sendMessage(message)
     }
   }
 
-  // création du colorpicker
+  // INITIALISATIONS
   iniColorPick() {
     let colorbutton = document.getElementById('colorpicker')!
     let colorpicker = colorbutton.querySelector('div')!
-
     for (let col of this.colorlist) {
       let d = document.createElement('div')
       d.classList.add('color')
       d.style.backgroundColor = col
-
       d.addEventListener('click', (e) => {
-            e.stopPropagation()
-            this.usercolor = col;
-            colorbutton.style.backgroundColor = this.usercolor
-            colorpicker.classList.add("hidd")
-          })
-
+        e.stopPropagation()
+        this.user.color = col;
+        this._socketService.userColor(this.user)
+        colorbutton.style.backgroundColor = this.user.color
+        colorpicker.classList.add("hidd")
+      })
       colorpicker.append(d)
     }
-    
-    if (this.usercolor === '') {
-      this.usercolor = this.colorlist[Math.floor(Math.random()*this.colorlist.length)]
-    }
-    colorbutton.style.backgroundColor = this.usercolor
   }
   
   iniTextarea() {
--- src/app/socketio.service.ts
+++ src/app/socketio.service.ts
@@ -12,21 +12,38 @@ export class SocketioService {
   socket:any;
 
   private message$ = new Subject<Message>()
+  private users$ = new Subject<any>()
 
   constructor() { }
 
-  sendMessage(message:Message) {
-    this.socket.emit('message0', message)
-  }
+  // FONCTIONS D’OBSERVABLES
   watchMessages() {
     return this.message$.asObservable()
+  }
+  watchUsers() {
+    return this.users$.asObservable()
+  }
+
+  sendMessage(message:Message) {
+    this.socket.emit('send0', message)
+  }
+  userCo(user:any) {
+    this.socket.emit('join0', user)
+  }
+  userDeco(user:any) {
+    this.socket.emit('leave0', user)
+  }
+  userColor(user:any) {
+    this.socket.emit('color0', user)
   }
 
   setupSocketConnection() {
     this.socket = io(environment.SOCKET_ENDPOINT)
-    this.socket.on('emission0', (data:any) => {
-      // console.log(data)
-      this.message$.next(data)
+    // écoute de la diffusion des messages
+    this.socket.on('msg0', (msg:any) => { this.message$.next(msg) })
+    // écoute de la diffusion de la liste des utilisateurs
+    this.socket.on('user0', (userlist:any) => { 
+      this.users$.next(userlist) 
     })
   }
   disconnect(){
